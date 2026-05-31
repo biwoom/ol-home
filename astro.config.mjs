bdfb9e204719211f870dda82b734b6c1a8f91b59
biwoom <bingeoul@gmail.com>
2026-05-31 18:13:49 +0900

    수정:마크다운 렌더링 스타일. 추가:각주기능

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import remarkGfm from 'remark-gfm';
 
 import mdx from '@astrojs/mdx';
 
@@ -10,6 +11,14 @@ export default defineConfig({
 
   // 저장소 이름
   base: '/ol-home',
+
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    remarkRehype: {
+      footnoteLabel: '각주',
+      footnoteBackLabel: '본문으로 돌아가기',
+    },
+  },
 
   vite: {
     plugins: [tailwindcss()],
